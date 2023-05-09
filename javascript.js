e8e9f5e59e9acd4bbc38dadf4d66b96c829d2527
nedajahanfar <130290258+nedajahanfar@users.noreply.github.com>
2023-05-09 22:37:37 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', showBox);
+
+function showBox() {
+    const triggerBottom = window.innerHeight /1.24;
+     
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+        if (boxTop < triggerBottom) {
+            box.classList.add('show');
+        } else {
+            box.classList.remove('show');
+        }
+    });
+}
